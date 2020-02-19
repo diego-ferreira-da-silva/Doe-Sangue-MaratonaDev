7900332fbf2335e6174b9168793dabf81312b8c5
Diego Ferreira da Silva <60447755+diegofds92@users.noreply.github.com>
2020-02-19 17:09:16 -0300

    Add files via upload
    Mudanças finais na aplicação do MaratonaDev

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 # Doe-Sangue-MaratonaDev
 Pagina Web Doe sangue, <b>MaratonaDev RockSeat.</b>
 
-Este Projeto foi desenvolvido durante a MaratonaDev 3ª edição
+Este Projeto foi desenvolvido durante a MaratonaDev 3ª edição da Rocketseat
+Este projeto é um cadastro para doadores de sangue, foi utilizado além a estrutura de HTML, estilização do CSS e Interação do JS o banco de dados cadastrais com o PostgreSQL
 
-Este projeto é um cadastro para você, que deseja ser um Doador de Sangue e salvar muitas Vidas!
+Foram Utilizados nesta aplicação:
+Programas:
+Visual Studio Code
+Google Chrome
+PostBird
+PostgreSQL
 
-Este é meu primeiro projeto nesse novo mundo do desenvolvimento.
+Frameworks utilizados foram: 
+Express
+Nodemun
+Nunjuks
+pg
+
+Aplicação desenvolvida com:
+HTML 5
+CSS
+JavaScript
+Postgree 
+Postbird
+
+Muito obrigado pela visita e espero que goste.
---
+++ server.js
@@ -0,0 +1,72 @@
+//Configurando Servidor.
+const express = require("express")
+const server = express()
+
+//Configurar Servidor para apresentar arquivos estáticos
+server.use(express.static('public'))
+
+//Habilitar body do formulário
+server.use(express.urlencoded({extended: true}))
+
+//Configurar conexão com o Banco de Dados.
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: 'p0sitiv0',
+    host: 'localhost',
+    port: 5432,
+    database: 'doesangue'
+})
+
+//Configurando a tamplete engine
+const nunjucks = require("nunjucks")
+    nunjucks.configure("./",{
+        express: server,
+        noCache: true, //Boolean ou Booleano = true or false
+    })
+
+
+//Configurar Apresentação da Página
+server.get("/", function(req, res){
+    
+    db.query("SELECT * FROM donors", function(err, result){
+        if (err) return res.send("Erro no Banco de Dados")
+
+        const donors = result.rows
+        return res.render("index.html", {donors })
+    })
+})
+
+server.post("/", function(req, res) {
+//req vai Pegar os dados do Formulário.
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    //Estrutura Condicional
+    //Fluxo Ideal
+    if(name == "" || email == "" || blood == ""){
+        return res.send("Todos os campos são Obrigatórios.")
+    }
+
+    //Colocando valores do Banco de Dados
+    const query = `
+        INSERT INTO donors ("name", "email", "blood") 
+        VALUES ($1, $2, $3)`
+
+    const values = [name, email, blood]
+
+    db.query(query, values, function(err){
+        //Fluxo de Erro.
+        if (err) return res.send("Erro no Banco de Dados")
+        
+        //Fluxo ideal
+        return res.redirect("/")
+    })
+
+})
+
+//Ligar o Servidor e Permitir Acesso na porta 3000
+server.listen(3000, function(){
+    console.log("Iniciei o Servidor.")
+})
